92c35f776cc9d596a26fac83f1b01cba99169e69
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-12 19:36:25 +0300

    Added methods for Location model

--- my_gallery/models.py
+++ my_gallery/models.py
@@ -18,6 +18,23 @@ class Location(models.Model):
     name = models.CharField(max_length =30)
     def _str_(self):
         return self.name
+    
+    def save(self):
+        self.save()
+
+    def delete(self):
+        self.delete()
+
+    @classmethod
+    def update(cls, pk, location):
+        cls.objects.filter(pk=pk).update(name=location)
+
+    @classmethod
+    def get_location(cls):
+        locations = Location.objects.all()
+        return locations
+
+
 
 class Photos(models.Model):
     name = models.CharField(max_length =30)
