f19c2c4b9aa77497839757bc03b9873b0cf615b6
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-12 17:29:53 +0300

    Added navbar

--- gallery/settings.py
+++ gallery/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'my_gallery.apps.MyGalleryConfig',
     'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
