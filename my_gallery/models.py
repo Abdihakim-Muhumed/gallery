bc890b69323234ca66168333d972d058c457b156
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-12 19:17:08 +0300

    Adde Photo model

--- my_gallery/models.py
+++ my_gallery/models.py
@@ -9,3 +9,13 @@ class Location(models.Model):
     name = models.CharField(max_length =30)
     def _str_(self):
         return self.name
+
+class Photo(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.CharField(max_length =150)
+    category = models.ManyToManyField(Category)
+    location = models.ManyToManyField(Location)
+
+    def _str_(self):
+        return self.name
+        
